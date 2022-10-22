49379fc838c1e02520455f7288c30b957a7c866b
wylden-star <64203014+wylden-star@users.noreply.github.com>
2022-10-22 10:29:56 +0800

    Update login-setting.sh

--- login-setting.sh
+++ login-setting.sh
@@ -9,8 +9,8 @@
 #1. Modify default IP  配置登录IP
 sed -i 's/192.168.1.1/192.168.88.88/g' openwrt/package/base-files/files/bin/config_generate
 
-#2. Clear the login password  清除登录密码
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+#2. Clear the login password  清除登录密码，默认登陆密码为password
+#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
